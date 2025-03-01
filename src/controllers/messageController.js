a987aa3e2a1ac67c3725758bc639d0aa293dbc0e
anxyum <115811968+anxyum@users.noreply.github.com>
2025-03-01 19:07:21 +0100

    added deleteMessage function

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -13,27 +13,149 @@ function handleMessage(data, req, res) {
     return;
   }
 
-  db.run(
-    "INSERT INTO messages (content, author_id, channel_id) VALUES (?, ?, ?)",
-    [data.message, user.user_id, data.channel_id],
-    function (err) {
+  db.get(
+    "SELECT * FROM channels WHERE id = ?",
+    [data.channel_id],
+    (err, channel) => {
       if (err) {
         logger.error(err);
         res.writeHead(500, { "Content-Type": "application/json" });
         res.end(
           JSON.stringify({
-            error: "Erreur lors de l'enregistrement du message",
+            error: "Erreur lors de la récupération du channel",
           })
         );
-      } else {
-        logger.info("Message enregistré avec succès !");
-        res.writeHead(201, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({ response: "Message envoyé !", id: this.lastID })
-        );
+        return;
       }
+
+      if (!channel) {
+        logger.warn("Channel non trouvé");
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Channel non trouvé" }));
+        return;
+      }
+
+      db.get(
+        "SELECT * FROM guild_users WHERE guild_id = ? AND user_id = ?",
+        [channel.guild_id, user.user_id],
+        (err, member) => {
+          if (err) {
+            logger.error(err);
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.end(
+              JSON.stringify({
+                error:
+                  "Erreur lors de la vérification de l'appartenance à la guild",
+              })
+            );
+            return;
+          }
+
+          if (!channel.public && !member) {
+            logger.warn("Channel privé et utilisateur non membre de la guild");
+            res.writeHead(403, { "Content-Type": "application/json" });
+            res.end(
+              JSON.stringify({
+                error:
+                  "Ce channel est privé et vous n'êtes pas membre de la guild",
+              })
+            );
+            return;
+          }
+
+          db.run(
+            "INSERT INTO messages (content, author_id, channel_id) VALUES (?, ?, ?)",
+            [data.message, user.user_id, data.channel_id],
+            function (err) {
+              if (err) {
+                logger.error(err);
+                res.writeHead(500, { "Content-Type": "application/json" });
+                res.end(
+                  JSON.stringify({
+                    error: "Erreur lors de l'enregistrement du message",
+                  })
+                );
+                return;
+              }
+
+              logger.info("Message enregistré avec succès !");
+              res.writeHead(201, { "Content-Type": "application/json" });
+              res.end(
+                JSON.stringify({
+                  response: "Message envoyé !",
+                  id: this.lastID,
+                })
+              );
+            }
+          );
+        }
+      );
     }
   );
 }
 
-module.exports = { handleMessage };
+function deleteMessage(data, req, res) {
+  logger.info(`deleting message`);
+
+  const user = authenticateToken(req);
+  if (!user) {
+    logger.warn("Accès refusé, token invalide");
+    res.writeHead(401, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Accès refusé, token invalide" }));
+    return;
+  }
+
+  db.get(
+    "SELECT * FROM messages WHERE id = ?",
+    [data.message_id],
+    (err, message) => {
+      if (err) {
+        logger.error(err);
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({
+            error: "Erreur lors de la récupération du message",
+          })
+        );
+        return;
+      }
+
+      if (!message) {
+        logger.warn("Message non trouvé");
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Message non trouvé" }));
+        return;
+      }
+
+      if (message.author_id !== user.user_id) {
+        logger.warn("Accès refusé, vous n'êtes pas l'auteur du message");
+        res.writeHead(403, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({
+            error: "Accès refusé, vous n'êtes pas l'auteur du message",
+          })
+        );
+        return;
+      }
+
+      db.run(
+        "UPDATE messages SET deleted = 1 WHERE id = ?",
+        [data.message_id],
+        (err) => {
+          if (err) {
+            logger.error(err);
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.end(
+              JSON.stringify({
+                error: "Erreur lors de la suppression du message",
+              })
+            );
+            return;
+          }
+        }
+      );
+    }
+  );
+}
+
+module.exports = { handleMessage, deleteMessage };
