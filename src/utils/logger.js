ee1f9c4a2ece0d2d3c2a10f671567d50fa897604
anxyum <115811968+anxyum@users.noreply.github.com>
2025-03-02 03:21:55 +0100

    better logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,25 @@
 const bunyan = require("bunyan");
+const { parse } = require("dotenv");
+
+const colors = {
+  INFO: "\x1b[32m", // Vert
+  WARN: "\x1b[33m", // Jaune
+  ERROR: "\x1b[31m", // Rouge
+  RESET: "\x1b[0m", // Reset la couleur
+};
+
+const consoleStream = {
+  level: "info",
+  stream: {
+    write: function (rec) {
+      const logObject = JSON.parse(rec);
+      const levelName = bunyan.nameFromLevel[logObject.level].toUpperCase();
+      const color = colors[levelName] || colors.RESET;
+      console.log(`${color}${levelName}: ${colors.RESET}${logObject.msg}`);
+    },
+  },
+};
+
 const logger = bunyan.createLogger({
   name: "platy-chat",
   streams: [
@@ -10,6 +31,7 @@ const logger = bunyan.createLogger({
       level: "error",
       path: "./logs/error.log",
     },
+    consoleStream,
   ],
 });
 
